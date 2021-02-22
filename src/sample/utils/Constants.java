08d389ad87c4194fb69efd948af61bde4a261519
algirdaskuslys <algirdas.kuslys@kitm.lt>
2021-02-22 12:15:47 +0200

    Pridėdatas dashboard langas su stulpeliais (#2)
    Co-authored-by: Zydrunas-Sir <73161629+Zydrunas-Sir@users.noreply.github.com>

---
+++ src/sample/utils/Constants.java
@@ -0,0 +1,10 @@
+package sample.utils;
+
+public class Constants {
+
+    public static final int DASHBOARD_WINDOW_WIDTH = 1920;
+    public static final int DASHBOARD_WINDOW_HEIGHT = 800;
+    public static final int LOGIN_REGISTER_WINDOW_WIDTH = 500;
+    public static final int LOGIN_REGISTER_WINDOW_HEIGHT = 300;
+
+}
