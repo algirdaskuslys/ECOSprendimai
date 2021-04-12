016d8721e8789cbfac403a4f99bc0139528ca4b7
algirdaskuslys <algirdas.kuslys@kitm.lt>
2021-04-12 13:21:56 +0300

    added excel file opener (#5)
    Co-authored-by: Dowydass <Dowydass@users.noreply.github.com>

--- src/sample/controller/DashboardController.java
+++ src/sample/controller/DashboardController.java
@@ -1,18 +1,32 @@
 package sample.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import sample.JPA.ProductCatalog;
+import sample.JPA.ProductCatalogDAO;
+import sample.JPA.ReadExcelWithProductCatalog;
 import sample.Main;
 import sample.utils.Constants;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 
@@ -21,6 +35,7 @@ public class DashboardController extends Main implements Initializable {
     public Button close_button;
     public TreeView<String> product_catalog_tree;
 
+    public Button open_file;
 
     public void goBackToLogin(ActionEvent actionEvent) {
         try {
@@ -44,7 +59,7 @@ public class DashboardController extends Main implements Initializable {
         stage.close();
     }
 
-    public void loadsProductsToCatalogTree(){
+    public void loadsProductsToCatalogTree() {
         TreeItem<String> root = new TreeItem<>("Visa klasifikacija");
 
         TreeItem<String> nodeA = new TreeItem<>("Moduline apsaugos aparatura");
@@ -84,8 +99,103 @@ public class DashboardController extends Main implements Initializable {
     }
 
 
+    @FXML
+    private TableView table;
+
+    public void loadDataToTable() {
+
+        TableColumn id = new TableColumn("Id");
+        TableColumn upload = new TableColumn("catalogNo");
+        TableColumn position = new TableColumn("symbol");
+        TableColumn employer = new TableColumn("priceNet");
+        TableColumn pay = new TableColumn("stock");
+
+        table.getColumns().addAll(id, upload, position, employer, pay);
+
+        ArrayList<ProductCatalog> productCatalogs = ProductCatalogDAO.displayAllItems();
+        System.out.println(productCatalogs);
+
+
+        // ProductCatalogDAO.find();
+
+
+        final ObservableList<ProductCatalog> data = FXCollections.observableArrayList(
+
+                new ProductCatalog(2020014, "Developer", 15, 2500, 1),
+                new ProductCatalog(2020014, "Developer", 15, 2500, 1)
+        );
+
+        id.setCellValueFactory(new PropertyValueFactory<ProductCatalog, String>("Id"));
+        upload.setCellValueFactory(new PropertyValueFactory<ProductCatalog, String>("catalogNo"));
+        position.setCellValueFactory(new PropertyValueFactory<ProductCatalog, String>("symbol"));
+        employer.setCellValueFactory(new PropertyValueFactory<ProductCatalog, String>("priceNet"));
+        pay.setCellValueFactory(new PropertyValueFactory<ProductCatalog, String>("stock"));
+
+        table.setItems(data);
+    }
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         loadsProductsToCatalogTree();
+/*
+        List<ProductCatalog> products = null;
+        try {
+            products = ReadExcelWithProductCatalog.readFileUsingPOI();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (ProductCatalog product : products) {
+            System.out.println(product.toString());
+        }
+        loadDataToTable();
+
+ */
     }
+
+    public void openExcelFileFromDialog() {
+        final FileChooser fileChooser = new FileChooser();
+        open_file.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent e) {
+                configureFileChooser(fileChooser);
+                File file = fileChooser.showOpenDialog(new Stage());
+                if (file != null) {
+                    openFile(file);
+                }
+            }
+        });
+
+    }
+
+    private void openFile(File file) {
+
+
+        List<ProductCatalog> products = null;
+        try {
+            products = ReadExcelWithProductCatalog.readFileUsingPOI(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (ProductCatalog product : products) {
+            System.out.println(product.toString());
+        }
+   //     loadDataToTable();
+
+    }
+
+    private static void configureFileChooser(
+            final FileChooser fileChooser) {
+        fileChooser.setTitle("Uzkrauti excel faila");
+        fileChooser.setInitialDirectory(
+                new File(System.getProperty("user.home"))
+        );
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Excel file", "*.xlsx")
+        );
+    }
+
 }
+
+
