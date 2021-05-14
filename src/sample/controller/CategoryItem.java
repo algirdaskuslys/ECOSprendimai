8269b01970eae623274832399437c96e3b37bd0c
algirdaskuslys <algirdas.kuslys@kitm.lt>
2021-05-14 09:14:20 +0300

    Pabaigtas treeview filtravimas. (#16)
    Co-authored-by: Zydrunas-Sir <73161629+Zydrunas-Sir@users.noreply.github.com>

---
+++ src/sample/controller/CategoryItem.java
@@ -0,0 +1,18 @@
+package sample.controller;
+
+class CategoryItem {
+    public String name;
+
+    public CategoryItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
